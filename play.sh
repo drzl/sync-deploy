da99a7e208ed33bad5e321401aef57d6dc67b179
inoremap <inoremap@gmail.com>
2021-12-21 11:34:21 +0500

    Sync upstream

--- play.sh
+++ play.sh
@@ -6,11 +6,22 @@ if [[ "X$1" == "Xv" ]]; then
     export ANSIBLE_NOCOLOR=True
 fi
 
+f=
+if [[ "X$1" == "Xf" ]]; then
+    f='--tags fast'
+fi
+
+s="--skip-tags apt-update"
+if [[ "X$1" == "Xfull" ]]; then
+    s=
+fi
+
 export ANSIBLE_CACHE_PLUGIN=jsonfile
 export ANSIBLE_CACHE_PLUGIN_CONNECTION=./cache
 export ANSIBLE_PIPELINING=1
 export ANSIBLE_SSH_PIPELINING=1
+#export ANSIBLE_DEPRECATION_WARNINGS=0
 
 #rm -f ~/.ssh/known_hosts
 
-ansible-playbook $v -i inv.yml --extra-vars @var.yml --extra-vars @pass.yml sync.yml
+ansible-playbook $v $f $s -i inv.yml --extra-vars @var.yml --extra-vars @pass.yml sync.yml
--- templates/config_pws_sync.php
+++ templates/config_pws_sync.php
@@ -1,9 +1,9 @@
 <?php
 
-$config_pws_sync__recv_db_base_url = '{{ sync_pub_prefix_f }}/recv_db';
-$config_pws_sync__register_db_base_url = '{{ sync_pub_prefix_f }}/register_db';
-$config_pws_sync__deploy_db_base_url = '{{ sync_pub_prefix_f }}/deploy_db';
-$config_pws_sync__status_db_base_url = '{{ sync_pub_prefix_f }}/status_db';
+$config_pws_sync__recv_db_base_url = '{{ sync_pub_prefix }}/recv_db';
+$config_pws_sync__register_db_base_url = '{{ sync_pub_prefix }}/register_db';
+$config_pws_sync__deploy_db_base_url = '{{ sync_pub_prefix }}/deploy_db';
+$config_pws_sync__status_db_base_url = '{{ sync_pub_prefix }}/status_db';
 $config_pws_sync__db_dir = '/opt/sync/db';
 $config_pws_sync__backup_dir = '/opt/sync/backup';
 $config_pws_sync__db_pwd = '{{ sync_fb_password }}';
@@ -11,13 +11,22 @@ $config_pws_sync__log_dir = '/opt/sync/log';
 $config_pws_sync__tmp_dir = '/var/tmp/pws_sync';
 $config_pws_sync__domain = '{{ sync_int_dom }}';
 $config_pws_sync__public_prefix = '{{ sync_pub_prefix }}/';
-$config_pws_sync__api_user = 'http://{{ sync_api_dom_f }}/api/user';
-#$config_pws_sync__api_user_unrestricted = true;
+$config_pws_sync__api_user = 'http://sync-f-api.virbr.{{ sync_int_dom }}/api/user';
+
+//$config_pws_sync__api_user_unrestricted = true;
 $config_pws_sync__api_user_unrestricted = false;
-$config_pws_sync__api_base = 'http://{{ sync_api_dom_f }}/api/base';
+
+$config_pws_sync__api_base = 'http://sync-f-api.virbr.{{ sync_int_dom }}/api/base';
 $config_pws_sync__start_ui = '{{ sync_pub_prefix_f }}/sync_ui';
 $config_pws_sync__error_ui = '{{ sync_pub_prefix }}/error_ui';
 $config_pws_sync__db_host = 'sync.{{ sync_int_dom }}';
-$config_pws_sync__mode = 'msg-session';
+
+//$config_pws_sync__mode = 'debug';
+$config_pws_sync__mode = 'release';
+
 $config_pws_sync__msg_session_open_url = '{{ sync_pub_prefix }}/open';
 $config_pws_sync__db_template = 'std-latest';
+
+$config_pws_sync__api_replica = '{{ sync_replica_api }}';
+$config_pws_sync__api_replica__read_only = true;
+$config_pws_sync__replica_db_dir = '/opt/sync/db';
